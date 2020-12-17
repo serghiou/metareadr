89c41fbd65c035ab141060524416b6c165e996b5
Stelios Serghiou <stelios.serghiou@gmail.com>
2020-12-17 00:52:59 -0800

    Update to correct typos, improve argument names, not require a file name by default, to remove the namespace and to not return a NULL.

--- R/mt_read_pmcoa.R
+++ R/mt_read_pmcoa.R
@@ -1,18 +1,21 @@
 #' Download file from PubMed Central
 #'
-#' Download the file of the designated PMCID in the file type of interest. This
-#'     uses the [OAI-PMH service](https://www.ncbi.nlm.nih.gov/pmc/tools/oai/).
+#' Download the file of the designated PMCID (PubMed Central ID) in the file
+#'     type of interest. This uses the
+#'     [OAI-PMH service](https://www.ncbi.nlm.nih.gov/pmc/tools/oai/).
 #'     The Open Access Index only maintains the full text for a subset of the
 #'     articles on PMC (not sure which subset).
 #'
-#' @param pmcid The PMCID of the article as a string or integer (e.g. 13900).
-#' @param file The file name, as a string, without the file type (e.g.
-#'     "my_file.xml").
+#' @param pmcid The PMCID (not PMID) of the article as a string or integer
+#'     (e.g. 13900).
+#' @param file_name The file name, as a string (e.g. "my_folder/my_file.xml").
+#'     If NULL (default), it will save the file in the current directory under
+#'     its PMCID (e.g. "PMC 13900").
 #' @param file_format Format as described on the OAI-PMH service. The default
 #'     ("pmc") downloads the whole text in XML format.
 #' @return Save the PubMed result as an XML file.
 #' @export
-mt_read_pmcoa <- function(pmcid, file, file_format = "pmc") {
+mt_read_pmcoa <- function(pmcid, file_name = NULL, file_format = "pmc") {
 
   base <- "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord"
   identifier <- "&identifier=oai:pubmedcentral.nih.gov:"
@@ -29,7 +32,7 @@ mt_read_pmcoa <- function(pmcid, file, file_format = "pmc") {
 
   if (!!length(pmc_error)) {
 
-    return(paste("Error in", pmcid, "-", pmc_error))
+    stop(paste("Error in", pmcid, "-", pmc_error))
 
   }
 
@@ -38,6 +41,12 @@ mt_read_pmcoa <- function(pmcid, file, file_format = "pmc") {
   pmc_record %>%
     xml2::xml_find_all(xpath = "//d1:metadata") %>%
     xml2::xml_contents() %>%
-    xml2::write_xml(paste0(file, ".xml"))
+    xml2::xml_ns_strip()
 
+  if (is.null(file_name)) {
+    file_name = paste0("PMC ", pmcid, ".xml")
+  }
+
+  # Save
+  invisible(xml2::write_xml(pmc_record, paste0(file_name, ".xml")))
 }
