c65b6c55e6db98abca8d5b3dac619b1526b05ef5
Stelios Serghiou <stelios.serghiou@gmail.com>
2020-12-17 00:51:52 -0800

    Create a package file.

---
+++ R/metareadr.R
@@ -0,0 +1,7 @@
+#' metareadr: Download data for meta-research.
+#'
+#' It provides an interface to easily download data to facilitate meta-research.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+"_PACKAGE"
